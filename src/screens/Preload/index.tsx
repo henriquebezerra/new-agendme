aa368b23dc50cfbd8dc1b2621cc6de216b1abaf6
Henrique Bezerra <ext.hbezerra@cruzeirodosul.edu.br>
2025-08-22 10:56:50 -0300

    feat: nova tela de load com animação

--- src/screens/Preload/index.tsx
+++ src/screens/Preload/index.tsx
@@ -1,38 +1,63 @@
-import { Container, AgendMeLabel, AgendMeLabelArea, LogoArea } from "@/screens/Preload/style";
-import { useContext, useEffect } from "react";
+import { Container, AgendMeLabel, AgendMeLabelArea, LogoArea, styles } from "@/screens/Preload/style";
+import { useContext, useEffect, useRef } from "react";
 import { useNavigation } from "@react-navigation/native";
-import ImageLogo from "@/components/ImageLogo";
 import { PreloadAction } from "./actions";
 import { UserContext } from "@/contexts/UserContext";
 import { PreloadScreenProp } from "@/types/general-type";
 import { AppointmentIcon } from "@/constants/icons";
-
+import { Animated } from 'react-native';
 
 const Preload = () => {
 
   const navigation = useNavigation<PreloadScreenProp>();
   const { dispatch: userDispatch } = useContext(UserContext);
   const service = new PreloadAction(userDispatch);
+  const highlightAnim = useRef(new Animated.Value(1)).current;
 
   useEffect(() => {
+    startAnimation();
     service.checkToken().then(() => {
       navigation.reset({routes:[{name:'UserNavigator'}]});
     }).catch(error => {
       navigation.reset({routes:[{name:'SignIn'}]});
     });
-  }, []);
+  }, [highlightAnim]);
+
+  const startAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(highlightAnim, {
+          toValue: 1.5,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(highlightAnim, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }
+
 
   return (
     <Container>
       <AgendMeLabelArea>
-        <AgendMeLabel>AgendMe</AgendMeLabel>
+        <AgendMeLabel
+          style={[
+            styles.agendMeLabel,
+            { transform: [{ scale: highlightAnim }] },
+          ]}
+        >
+          AgendMe
+        </AgendMeLabel>
       </AgendMeLabelArea>
       <LogoArea>
         <AppointmentIcon size={30} color="#6c6e6cff"/>
       </LogoArea>
     </Container>
   );
-
 }
 
 export default Preload;
--- src/screens/Preload/style.ts
+++ src/screens/Preload/style.ts
@@ -1,18 +1,18 @@
 import styled from "styled-components/native";
+import { Animated, StyleSheet } from 'react-native';
 
 export const Container = styled.SafeAreaView`
   background-color: #63C2D1;
   flex: 1;
   align-items: center;`;
 
-export const AgendMeLabel = styled.Text`
+export const AgendMeLabel = Animated.createAnimatedComponent(styled.Text`
   color: #6c6e6cff;
-  font-size: 50px;
   text-align: center;
   font-family: 'Barrio_400Regular';
   width: 100%;
   height: 100px;
-`;
+`);
 
 export const AgendMeLabelArea = styled.View`
   justify-content: flex-end;
@@ -29,3 +29,10 @@ export const LogoArea = styled.View`
   position: absolute;
   padding-bottom: 23px;
 `;
+
+export const styles = StyleSheet.create({
+  agendMeLabel: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+});
